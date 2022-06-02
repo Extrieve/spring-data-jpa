fff2c5f9176ba20f18e5105a5fcd13141fcc8f2e
Nick <nicolas.cinera@gmail.com>
2022-06-02 19:47:01 -0400

    One to One Entity Relationship

---
+++ src/test/java/com/extrieve/spring/data/jpa/repository/CourseMaterialRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.extrieve.spring.data.jpa.repository;
+
+import com.extrieve.spring.data.jpa.entity.Course;
+import com.extrieve.spring.data.jpa.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseMaterialRepositoryTest {
+
+    @Autowired
+    private CourseMaterialRepository courseMaterialRepository;
+
+    @Test
+    public void saveCourseMaterial() {
+        Course course = Course.builder()
+                .courseTitle("Java")
+                .courseCredits(3)
+                .build();
+
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .courseUrl("https://www.udemy.com/course/java-spring-boot-development/")
+                .course(course)
+                .build();
+
+        courseMaterialRepository.save(courseMaterial);
+    }
+
+}
---
+++ src/test/java/com/extrieve/spring/data/jpa/repository/CourseRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.extrieve.spring.data.jpa.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+}
